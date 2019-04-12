afaafb778f180d741c39fd386f527777460e8660
Amish Naidu <amhndu@gmail.com>
2019-04-12 17:21:06 +0530

    Add parser for basic messages

---
+++ quirc/src/parser.rs
@@ -0,0 +1,162 @@
+#[derive(Debug, PartialEq)]
+enum Prefix<'a> {
+    Server {
+        host: &'a str,
+    },
+    User {
+        nick: &'a str,
+        username: Option<&'a str>,
+        host: &'a str,
+    },
+}
+
+#[derive(Debug)]
+struct Message<'a> {
+    prefix: Option<&'a [u8]>,
+    command: &'a [u8],
+    params: Vec<&'a [u8]>,
+}
+
+const SPACE: char = ' ';
+const COLON: char = ':';
+
+fn to_prefix_server(input: &str) -> Prefix {
+    Prefix::Server { host: input }
+}
+
+fn is_specialalphanum(c: u8, special: &[u8]) -> bool {
+    special.contains(&c) || nom::is_alphanumeric(c)
+}
+
+named!(host_parser<&[u8], &str>,
+    map_res!(
+        take_while!(|c| is_specialalphanum(c, b".")),
+        std::str::from_utf8
+    )
+);
+
+named!(nick_parser<&[u8], &str>,
+    map_res!(
+        take_while!(|c| is_specialalphanum(c, b"[]\\`_^{|}-")),
+        std::str::from_utf8
+    )
+);
+
+named!(server_prefix_parser<&[u8], Prefix>,
+    map!(host_parser, to_prefix_server)
+);
+
+named!(user_prefix_parser<&[u8], Prefix>,
+    do_parse!(
+       nick: nick_parser                    >>
+       username: opt!(
+           preceded!(
+               tag!(b"!"),
+               map_res!(nom::alphanumeric, std::str::from_utf8)
+           )
+       )                                    >>
+       tag!(b"@")                           >>
+       host: host_parser                    >>
+       (Prefix::User { nick, username, host } )
+    )
+);
+
+named!(prefix_parser<&[u8], Prefix>,
+    delimited!(
+           char!(COLON),
+           alt!(user_prefix_parser | server_prefix_parser ),
+           char!(SPACE)
+    )
+);
+
+named!(middle_params_parser<&[u8], Vec<&str> >,
+    many_m_n!(0, 14,
+        preceded!(
+            char!(SPACE),
+            map_res!(is_not!("\0\r\n :"), std::str::from_utf8)
+        )
+    )
+);
+
+named!(trailing_params_parser<&[u8], Option<&str> >,
+    opt!(
+        preceded!(
+            alt!(tag!(" :") | tag!(" ")),
+            map_res!(is_not!("\0\r\n"), std::str::from_utf8)
+        )
+    )
+);
+
+named!(params_parser<&[u8], Vec<&str>>,
+   do_parse!(
+       middle: middle_params_parser         >>
+       trailing: trailing_params_parser     >>
+       ({
+           let mut params = middle;
+           if let Some(trailing) = trailing {
+               params.push(trailing);
+           }
+
+           params
+       })
+   )
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prefix_test() {
+        assert_eq!(
+            prefix_parser(b":irc.example.com end").unwrap(),
+            (
+                &b"end"[..],
+                Prefix::Server {
+                    host: "irc.example.com"
+                }
+            )
+        );
+        assert_eq!(
+            prefix_parser(b":nick!on@example.com end").unwrap(),
+            (
+                &b"end"[..],
+                Prefix::User {
+                    nick: "nick",
+                    username: Some("on"),
+                    host: "example.com"
+                }
+            )
+        );
+        assert_eq!(
+            prefix_parser(b":nick@example.com end").unwrap(),
+            (
+                &b"end"[..],
+                Prefix::User {
+                    nick: "nick",
+                    username: None,
+                    host: "example.com"
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn params_test() {
+        assert_eq!(
+            middle_params_parser(b" first second third\r\n").unwrap(),
+            (&b"\r\n"[..], vec!["first", "second", "third"])
+        );
+        assert_eq!(
+            params_parser(b" first second third :last with spaces\r\n").unwrap(),
+            (
+                &b"\r\n"[..],
+                vec!["first", "second", "third", "last with spaces"]
+            )
+        );
+        assert_eq!(
+            params_parser(b" first :last:with colons\r\n").unwrap(),
+            (&b"\r\n"[..], vec!["first", "last:with colons"])
+        );
+    }
+}
